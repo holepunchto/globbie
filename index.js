136f8e627c67f7f7ed5c5cbfd3e63270dfc52553
Mikkel Malmberg <mikkel@brnbw.com>
2025-10-03 23:41:02 +0200

    Apply Prettier

--- index.js
+++ index.js
@@ -4,34 +4,38 @@ const picomatch = require('picomatch')
 const process = require('process')
 
 class Globbie {
-  constructor (pattern, { sync = false } = {}) {
-    this._isMatch = picomatch(pattern, { windows: process.platform === 'win32' })
+  constructor(pattern, { sync = false } = {}) {
+    this._isMatch = picomatch(pattern, {
+      windows: process.platform === 'win32'
+    })
     this._sync = sync
   }
 
-  match (dir = '.') {
+  match(dir = '.') {
     return this._sync ? this.#matchSync(dir) : this.#matchAsync(dir)
   }
 
-  #matchSync (dir = '.') {
+  #matchSync(dir = '.') {
     const matches = []
     for (const file of fs.readdirSync(dir)) {
       const filePath = path.join(dir, file)
-      if (fs.lstatSync(filePath).isDirectory()) matches.push(...this.match(filePath))
+      if (fs.lstatSync(filePath).isDirectory())
+        matches.push(...this.match(filePath))
       else if (this._isMatch(filePath)) matches.push(filePath)
     }
 
     return matches
   }
 
-  async #matchAsync (dir = '.') {
+  async #matchAsync(dir = '.') {
     const files = await fs.promises.readdir(dir)
 
     const matches = []
     const promises = files.map(async (file) => {
       const filePath = path.join(dir, file)
 
-      if ((await fs.promises.lstat(filePath)).isDirectory()) matches.push(...(await this.#matchAsync(filePath)))
+      if ((await fs.promises.lstat(filePath)).isDirectory())
+        matches.push(...(await this.#matchAsync(filePath)))
       else if (this._isMatch(filePath)) matches.push(filePath)
     })
 
